bdbc03c555abaf6da1b938179d31d8b624a00414
etamqx <talesaraujodossantos@gmail.com>
2017-06-25 16:44:01 -0300

    fix src file errors

--- src/profile/PessoaJuridica.java
+++ src/profile/PessoaJuridica.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package profile;
 
 public class PessoaJuridica extends Perfil {
@@ -18,24 +17,3 @@ public class PessoaJuridica extends Perfil {
 		this.cpnj = cpnj;
 	}
 }
-=======
-package profile;
-
-public class PessoaJuridica extends Perfil {
-	
-	private long cpnj;
-	
-	public PessoaJuridica(String usuario, long cpnj) {
-		super(usuario);
-		this.cpnj = cpnj;
-	}
-
-	long getCpnj() {
-		return this.cpnj;
-	}
-	
-	void setCpnj(long cpnj) {
-		this.cpnj = cpnj;
-	}
-}
->>>>>>> cb5eace0f48cf111b1fc9bfa48918417416a0648
