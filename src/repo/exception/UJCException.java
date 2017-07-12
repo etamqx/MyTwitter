0a6711adb2963e0be7336553c9b56905649b671a
etamqx <talesaraujodossantos@gmail.com>
2017-07-12 00:56:40 -0300

    added repository exceptions

---
+++ src/repo/exception/UJCException.java
@@ -0,0 +1,23 @@
+package repo.exception;
+
+public class UJCException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String usuario;
+	
+	
+	public UJCException(String usuario){
+		super("Usuário já cadastrado");
+		this.usuario = usuario;
+	}
+	
+	public String alert() {
+		return "O usuário '" + this.usuario + "' já está cadastrado.";
+	}
+	
+	public String getUsuario() {
+		return this.usuario;
+	}
+	
+}
---
+++ src/repo/exception/UNCException.java
@@ -0,0 +1,23 @@
+package repo.exception;
+
+public class UNCException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String usuario;
+	
+	
+	public UNCException(String usuario){
+		super("Usuário não cadastrado");
+		this.usuario = usuario;
+	}
+	
+	public String alert() {
+		return "O usuário '" + this.usuario + "' não está cadastrado.";
+	}
+	
+	public String getUsuario() {
+		return this.usuario;
+	}
+
+}
